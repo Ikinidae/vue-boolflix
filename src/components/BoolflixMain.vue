da6b275318c18076fb3fbc40f8b859d6eb49e5fc
Ikinidae <federicaelia24@gmail.com>
2022-06-14 18:05:41 +0200

    Creato metodo per salvare dati da API

---
+++ src/components/BoolflixMain.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <h1>Main</h1>
+    </div>
+</template>
+
+<script>
+import axios from "axios"
+
+export default {
+    name: 'BoolflixMain',
+    data() {
+        return {
+            apiUrl: "https://api.themoviedb.org/3/search/movie?api_key=e110a76546d14ef00629c28529c7b91b&language=it-IT&query=Fight+Club&page=1&include_adult=true",
+            filmList: []
+        }
+    },
+    created() {
+        this.getFilm();
+    },
+    methods: {
+        getFilm () {
+            axios
+                .get(this.apiUrl)
+                .then((result) => {
+                    this.filmList = result.data.results;
+                    console.log(this.filmList);
+                })
+                .catch((error) => {
+                    console.log("Errore", error);
+                })
+        }
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
