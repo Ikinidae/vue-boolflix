c080522d73e5dbfc03e6cfb06623932dbc21a9ab
Ikinidae <federicaelia24@gmail.com>
2022-06-15 11:33:26 +0200

    Aggiunta bandierina che cambia in base alla lingua

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -2,18 +2,23 @@
     <div>
         <h3>{{objectCard.title}}</h3>
         <p>{{objectCard.original_title}}</p>
-        <p>{{objectCard.original_language}}</p>
+        <p><lang-flag :iso="objectCard.original_language" /></p>
         <p>{{objectCard.vote_average}}</p>
+        
     </div>
 </template>
 
 <script>
+import LangFlag from 'vue-lang-code-flags';
+
 export default {
     name: 'ProductCard',
-
+    components: {
+        LangFlag
+    },
     props: {
         objectCard: Object
-    }
+    },
 }
 </script>
 
