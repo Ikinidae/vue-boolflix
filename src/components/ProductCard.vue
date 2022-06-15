3f9b74ac2a16640fbd64a049acfc4c3b2fb3c767
Ikinidae <federicaelia24@gmail.com>
2022-06-15 19:03:26 +0200

    Milestone 3 completata, resta un warn ignoto

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -3,8 +3,11 @@
         <img :src="'https://image.tmdb.org/t/p/w342' + objectCardFilm.poster_path" alt="">
         <h3>{{objectCardFilm.title}}</h3>
         <p>{{objectCardFilm.original_title}}</p>
+        <p>{{objectCardFilm.media_type}}</p>
         <p>Lingua originale: <lang-flag :iso="objectCardFilm.original_language" /></p>
-        <p>{{objectCardFilm.vote_average}}</p>
+        <p>Voto: {{Math.round(objectCardFilm.vote_average/2)}}</p>
+        <font-awesome-icon v-for="i in getStars(objectCardFilm.vote_average)" :key="i" icon="fa-solid fa-star"/>
+        <font-awesome-icon v-for="i in getEmptyStars(objectCardFilm.vote_average)" :key="i" icon="fa-regular fa-star"/>
     </div>
 </template>
 
@@ -19,6 +22,16 @@ export default {
     props: {
         objectCardFilm: Object,
     },
+    methods: {
+        getStars(vote) {
+            let calc = parseInt(Math.round(vote/2));
+            return calc;
+        },
+        getEmptyStars(vote) {
+            let pippo = parseInt(Math.round(5-(vote/2)));
+            return pippo;
+        }
+    }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+
 import LangFlag from 'vue-lang-code-flags';
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faStar as faStars } from '@fortawesome/free-regular-svg-icons'
+import { faStar } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faStars)
+library.add(faStar)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
  
 Vue.component('lang-flag', LangFlag);
 
